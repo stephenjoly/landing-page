848e2a50a9165ab9564ee859fae3deebcb06dff2
Stephen Joly <stephen.a.joly@gmail.com>
2026-04-01 13:40:02 -0400

    Prevent project logos from shrinking

--- src/components/ProjectsGallary.tsx
+++ src/components/ProjectsGallary.tsx
@@ -96,8 +96,13 @@ function ConsultingProjectsGrid({
       {projects.map((project) => (
         <Card as="li" key={project.name}>
           <div className="flex items-center space-x-3 pb-2">
-            <div className="relative z-10 flex h-12 w-12 items-center justify-center rounded-full bg-white shadow-md ring-1 shadow-zinc-800/5 ring-zinc-900/5 dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0">
-              <Image src={project.logo} alt="" className="h-8 w-8" unoptimized />
+            <div className="relative z-10 flex h-12 w-12 flex-none items-center justify-center rounded-full bg-white shadow-md ring-1 shadow-zinc-800/5 ring-zinc-900/5 dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0">
+              <Image
+                src={project.logo}
+                alt=""
+                className="h-8 w-8 object-contain"
+                unoptimized
+              />
             </div>
             <h2 className="flex-grow font-semibold text-zinc-800 dark:text-zinc-100">
               <Card.Button onClick={() => onProjectSelect(project)}>
